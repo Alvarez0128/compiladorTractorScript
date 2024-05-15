64495cf6c78e6b713c710e661240524564319207
Alvarez0128 <ceemalvarezji@ittepic.edu.mx>
2024-05-14 18:56:16 -0700

    Update MainServer.py

--- src/analyzers/MainServer.py
+++ src/analyzers/MainServer.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from Analizador_Lexico import construir_analizador_lexico, reiniciar_analizador_lexico, obtener_errores_lexico
-from Analizador_Sintactico import construir_analizador_sintactico, obtener_errores_sintactico, tree_to_string
+from Analizador_Sintactico import construir_analizador_sintactico, obtener_errores_sintactico, tree_to_json, tree_to_string
 
 app = Flask(__name__)
 CORS(app)
@@ -29,7 +29,7 @@ def compile_code():
             'column': tok.lexpos - code.rfind('\n', 0, tok.lexpos)
         })
 
-    arbol = tree_to_string(parser.parse(code))
+    arbol = tree_to_json(parser.parse(code))
     
     # Obtener errores después de analizar el código
     errores_lexico = obtener_errores_lexico()
